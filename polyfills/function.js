b8957e0face921bf36bc9e4191fe11cc69f8d54d
Hridayesh Sharma <dev.hridayesh@gmail.com>
2022-09-05 13:07:40 +0000

    Added call and apply polyfills

--- polyfills/function.js
+++ polyfills/function.js
@@ -3,9 +3,9 @@ Function.prototype.customCall = customCall;
 Function.prototype.customApply = customApply;
 
 // bind
-function customBind(thisArgs, ...args) {
+function customBind(thisArg, ...args) {
   const originalFunction = this;
-  const context = thisArgs;
+  const context = thisArg;
   const symbol = Symbol();
   context[symbol] = originalFunction;
   return function (...arguments) {
@@ -21,6 +21,30 @@ let boundFoo = foo.customBind({ name: 'Hridayesh' }, 1);
 console.log(boundFoo('Hridayesh'));
 
 // call
-function customCall() {}
+function customCall(thisArg, ...args) {
+  const originalFunction = this;
+  const context = thisArg;
+  const symbol = Symbol();
+  context[symbol] = originalFunction;
+  context[symbol](...args);
+}
+function foo(name, age) {
+  console.log({ age, name, this: this });
+}
+
+foo.customCall({ name: 'XYZ', age: 12 }, 'XZZ', 32);
+
 //apply
-function customApply() {}
+function customApply(thisArg, args) {
+  const originalFunction = this;
+  const context = thisArg;
+  const symbol = Symbol();
+  context[symbol] = originalFunction;
+  context[symbol](...args);
+}
+
+function foo(name, age, school) {
+  console.log({ age, name, school, this: this });
+}
+
+foo.customApply({ name: 'XYZ', age: 12 }, ['XZZ', 32, 'yyy']);
